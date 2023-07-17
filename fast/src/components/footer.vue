b3aaa5b465667e1d0727a2311da29fb65cb06754
shafikulislam <shafikul.18288@gamil.com>
2023-07-17 22:50:41 +0600

    last modify

---
+++ fast/src/components/footer.vue
@@ -0,0 +1,69 @@
+<script setup>
+const container = 'container'
+const row = 'row'
+const footerLink1 = 'product management'
+const footerLink2 = 'design / creatves'
+const footerLink3 = 'education & trainging'
+const footerLink4 = 'ul/ux designers'
+const footerLink5 = 'About Us'
+const footerLink6 = 'Partnerships'
+const footerLink7 = 'Finance Experts'
+const footerLink8 = 'Project Management'
+const Facebook = 'Facebook'
+const Twitter = 'Twitter'
+const Instagram = 'Instagram'
+const Linkedin = "Linkedin"
+const footerHeading = 'categories'
+const footerHeading2 = 'About'
+const footerHeading3 = 'Follow Us'
+const footerlink = 'text-decoration-none text-capitalize text-black'
+const footerhead = 'fs-4 font-bold mt-5 text-uppercase'
+
+const footerHead = 'Smart Mobile Application'
+const footerP = 'Automate your entine healthcare hiring, onboadrding and Compllance eith true technology platefrom'
+
+</script>
+<template>
+     <section>
+        <div :class="container">
+            <div :class="row">
+                <div class="col-md-3">
+                    <h2 :class="footerhead">{{ footerHead }}</h2>
+                    <p>{{ footerP }}</p>
+                    <div class="d-flex">
+                        <input type="text" placeholder="Enter Your Text here" class="footerInp">
+                        <button class="myBtn" type="submit">Registar</button>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h3 :class="footerhead">{{ footerHeading }}</h3>
+                    <ul class="list-unstyled">
+                        <li><a href="#" :class="footerlink">{{ footerLink1 }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ footerLink2 }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ footerLink3 }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ footerLink4 }}</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h3 :class="footerhead">{{ footerHeading2 }}</h3>
+                    <ul class="list-unstyled text-capitalize">
+                        <li><a href="#" :class="footerlink">{{ footerLink5 }} </a></li>
+                        <li><a href="#" :class="footerlink">{{ footerLink6 }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ footerLink7 }} </a></li>
+                        <li><a href="#" :class="footerlink">{{ footerLink8 }} </a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h3 :class="footerhead">{{ footerHeading3 }}  </h3>
+                    <ul class="list-unstyled text-capitalize">
+                        <li><a href="#" :class="footerlink">{{ Facebook }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ Twitter }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ Instagram }}</a></li>
+                        <li><a href="#" :class="footerlink">{{ Linkedin }}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped></style>
---
+++ fast/src/components/subscribe.vue
@@ -0,0 +1,26 @@
+<script setup>
+const subscribe = 'Subscribe to get updated'
+const subscribeP = 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.'
+const colmd2 = 'col-md-2'
+const maxWidth = 'max-width: 100%'
+const container = 'container'
+</script>
+<template>
+    <section>
+        <div :class="container">
+            <div class="row footerAge">
+                <div :class="colmd2"> 
+                    <img src="../img/mobile.png" :style="maxWidth" style=" height: 160px; " alt="">    
+                </div>
+                <div class="col-md-8 "> 
+                    <h1>{{ subscribe }}</h1>
+                    <p>{{ subscribeP }}</p>
+                </div>
+                <div :class="colmd2"> 
+                    <button class="myBtn">get start</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped></style>
