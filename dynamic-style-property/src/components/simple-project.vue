e55cd6951c59305feb679d5faffd51e98cb25d60
shafikulislam <shafikul.18288@gamil.com>
2023-07-22 19:06:17 +0600

    create light app

--- dynamic-style-property/src/components/simple-project.vue
+++ dynamic-style-property/src/components/simple-project.vue
@@ -21,6 +21,16 @@ const checking = computed(()=>{
     height:` ${allStyle.height}px`
   }
 })
+
+
+function resetFn(){
+  allStyle.perspective = 100
+  allStyle.rotateX = 0
+  allStyle.rotateY = 0
+  allStyle.rotateZ = 0
+  allStyle.width = 30
+  allStyle.height = 50
+}
 </script>
 <template>
   <h1 class="font-bold text-center mt-4 text-3xl font-serif">Pratice Project</h1>
@@ -61,7 +71,7 @@ const checking = computed(()=>{
       <div class=" w-24 h-24 bg-slate-300" :style="checking"> </div>
     </div>
   </div>
-  
+  <button @click="resetFn" class="w-32 ml-20 text-2xl font-bold capitalize border-2 border-gray-200 shadow-md shadow-indigo-300"> Reset</button>
 </template>
 <style scoped>
 /* .fast{
